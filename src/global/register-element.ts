480235f89fa16c5120c087218d2cd2acbde3dd1f
Ryuuzaki <1228950291@qq.com>
2021-12-11 10:49:45 +0800

    chore: 集成 element-plus

---
+++ src/global/register-element.ts
@@ -0,0 +1,27 @@
+import "element-plus/theme-chalk/base.css";
+import { App } from "vue";
+// 局部引入
+import {
+  ElAlert,
+  ElAside,
+  ElButton,
+  ElForm,
+  ElFormItem,
+  ElInput,
+  ElRadio
+} from "element-plus";
+const components = [
+  ElAlert,
+  ElAside,
+  ElButton,
+  ElForm,
+  ElFormItem,
+  ElInput,
+  ElRadio
+];
+
+export default function (app: App): void {
+  for (const component of components) {
+    app.component(component.name, component);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+// 文件夹下的 index 文件可以省略，会自己去找
+import { registerApp } from "./global";
 
-createApp(App).mount("#app");
+// 全局引入
+// import ElementPlus from "element-plus";
+// // 记得引入样式
+// import "element-plus/theme-chalk/index.css";
+
+import router from "./router";
+import store from "./store";
+
+const app = createApp(App);
+
+registerApp(app);
+
+app.use(router);
+app.use(store);
+// app.use(ElementPlus);
+app.mount("#app");
